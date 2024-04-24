df5268844efc72a578befb9c5378286f682410c2
Neia2 <nenaslife@gmail.com>
2024-04-24 22:29:52 +0200

    updating files

--- shell.h
+++ shell.h
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-#ifndef SHELL_H
-#define SHELL_H
-
-#define DELIMITER " \t"
-=======
 #ifndef MAIN_H
 #define MAIN_H
->>>>>>> matrix
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,32 +9,6 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <signal.h>
-<<<<<<< HEAD
-#include <errno.h>
-#include <stdarg.h>
-
-/*display_prompt.c*/
-void display_prompt(void);
-
-/*exit_function.c*/
-int _atoi(char *s);
-void exitt(char **arv);
-
-/*exec_com.c*/
-int custom_execlp(const char *file, const char *arg0, ...);
-void execute_command(const char *command);
-
-/*exit_function.c*/
-int _atoi(char *s);
-void exitt(char **arv);
-
-/*user_input.c*/
-void user_input(char *command, size_t size);
-void read_command(char *command, size_t size);
-void sigint_handler(int signum);
-
-#endif /* SHELL_H */
-=======
 #include <fcntl.h>
 #include <stdarg.h> 
 
@@ -95,4 +62,3 @@ char *_getpid(void);
 
 
 #endif
->>>>>>> matrix
